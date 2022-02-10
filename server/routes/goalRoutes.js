bb2b3af57b395fb5e7458491d45c7a7e8da4aa2c
AlexGA93 <alexgimat@hotmail.com>
2022-02-10 21:55:14 +0100

    User and Goal routes protected

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -5,9 +5,12 @@ const {
   setGoal,
   updateGoal,
   deleteGoal,
-} = require('../controllers/goalsController')
+} = require('../controllers/goalsController');
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+// protect routes
+const { protect } = require('../middleware/authMiddleware');
+
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 module.exports = router;
