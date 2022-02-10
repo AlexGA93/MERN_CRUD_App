0b961d0a51149f3a2ddbc4977a833b6fe4292c54
elros <alexgimat@hotmail.com>
2022-02-10 16:11:15 +0100

    users controller prepared to Mongo async requests

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,37 +1,38 @@
-// User's root logic
+// asyc handler
+const asyncHandler = require('express-async-handler');
 
 // @desc    Get users
 // @route   GET /api/users
 // @access  private
-const getUsers = (req, res) =>{
+const getUsers = asyncHandler(async (req, res) =>{
     res.status(200).json({message:'get users'});
-};
+});
 
 // @desc    Create users
 // @route   POST /api/users/:id
 // @access  private
-const postUsers = (req, res) =>{
+const postUsers = asyncHandler(async (req, res) =>{
     //error and exception handling
     if(!req.body.name){
         res.status(400)// .json({message:'Please add a name field'});
         throw new Error('Please add a name field')
     }
     res.status(200).json({message:'post users'});
-};
+});
 
 // @desc    Update users
 // @route   PUT /api/users/:id
 // @access  private
-const putUsers = (req, res) =>{
+const putUsers = asyncHandler(async (req, res) =>{
     res.status(200).json({message:`update user ${req.params.id}`});
-};
+});
 
 // @desc    Delete users
 // @route   DELETE /api/users?:id
 // @access  private
-const deleteUsers = (req, res) =>{
+const deleteUsers = asyncHandler(async (req, res) =>{
     res.status(200).json({message:`delete user ${req.params.id}`});
-};
+});
 
 //exportin methods
 module.exports = {getUsers, postUsers, putUsers, deleteUsers};
