5ca15fdb846f45b40c6034e122952747a8778114
elros <alexgimat@hotmail.com>
2022-02-10 15:27:50 +0100

    controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,21 +5,17 @@ const router = express.Router();
 // importing controllers
 const {getUsers, postUsers, putUsers, deleteUsers} = require('../controllers/usersController');
 
-// groups of routes
-router.route('/').get(getUsers).post(postUsers);
-router.route('/:id').put(putUsers).delete(deleteUsers);
+// get users route
+router.get('/', getUsers);
 
-// // get users route
-// router.get('/', getUsers);
+// porst/ create users route
+router.post('/', postUsers);
 
-// // porst/ create users route
-// router.post('/', postUsers);
+// update users route
+router.put('/:id', putUsers);
 
-// // update users route
-// router.put('/:id', putUsers);
-
-// // delete users route
-// router.delete('/:id', deleteUsers);
+// delete users route
+router.delete('/:id', deleteUsers);
 
 //exporting script
 module.exports = router;
