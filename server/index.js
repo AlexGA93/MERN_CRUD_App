c6c6f0a120c5e67a0ad3cbe29e3368323013b472
elros <alexgimat@hotmail.com>
2022-02-10 15:00:15 +0100

    first route

--- server/index.js
+++ server/index.js
@@ -7,4 +7,10 @@ const port = process.env.PORT | 5000;
 
 
 const app = express();
+// first route
+app.get('/api/users', (req, res) => {
+    res.status(200).json({message:'get users'})
+});
+
+
 app.listen(port, () => console.log(`Server running at port ${port}`));
